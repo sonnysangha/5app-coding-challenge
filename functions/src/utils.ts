dae55f90016b75f189acaf199b470169c498c226
Saajan Sangha <saajansangha@saajans-mbp-6.home>
2019-10-02 18:43:35 +0100

    minor fix

--- functions/src/utils.ts
+++ functions/src/utils.ts
@@ -2,10 +2,10 @@ import { Item, Logo } from "./interfaces";
 
 export const transformPayload = (payload: Item[]) => {
   // filter for response items with count greater than 1.
-  payload = payload.filter((item: Item) => item.count > 1);
+  const transformedPayload = payload.filter((item: Item) => item.count > 1);
 
   // get URL for thumbnail from payload item's list of logos no larger than 128x128 but no smaller than 64x64.
-  payload.forEach((item: Item) => {
+  transformedPayload.forEach((item: Item) => {
     item.logos.forEach((logo: Logo) => {
       const [width, height] = logo.size.split("x").map(char => parseInt(char));
       if (width >= 64 && width <= 128 && (height >= 64 && height <= 128)) {
@@ -16,5 +16,5 @@ export const transformPayload = (payload: Item[]) => {
     delete item.logos;
   });
 
-  return payload;
+  return transformedPayload;
 };
